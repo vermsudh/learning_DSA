fd74dd0365d52ebda4f3dfd0589254f50dcc1483
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-01 20:26:43 +0530

    Add C++ programs for input handling and basic arithmetic operations with supporting images

---
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/Programming/input_in_c/code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double price;
+    cout << "Enter your price" << endl;
+    cin >> price; //input from the user
+    cout << "Your entered price is " << price << endl;
+    return 0;
+}
+
---
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/Programming/operators/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5, b = 7;
+    cout << "Enter the value of a : " << endl;
+    cin >> a;
+    cout << "Enter the value of b : " << endl;
+    cin >> b;
+    cout << "The sum of a and b is : " << (a+b) << endl;
+    return 0; 
+}
+
