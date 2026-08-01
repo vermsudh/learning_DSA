888371a1710e30ee82b081835dd61b672283398c
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-01 20:39:31 +0530

    Add C++ program to calculate the sum of two numbers

---
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/Programming/exercise/2_sum/code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Print the sum of 2 numbers. WAP
+int main(){
+    int a,b;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+    cout << "THe sume of a and b is : " << (a+b) << endl;
+}
