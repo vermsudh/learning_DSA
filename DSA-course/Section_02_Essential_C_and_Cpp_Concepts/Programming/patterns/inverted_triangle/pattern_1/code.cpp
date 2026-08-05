b0deb2e06acb85f3c76aeb081c119477bd2e732c
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-05 21:06:46 +0530

    Add inverted triangle pattern program (pattern 2) with character output

--- DSA-course/Section_02_Essential_C_and_Cpp_Concepts/Programming/patterns/inverted_triangle/pattern_1/code.cpp
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/Programming/patterns/inverted_triangle/pattern_1/code.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main(){
-    int n; 
-    cout << "Enter the value of n to print from 1 to n :  ";
-    cin >> n;
-    
-    for(int i = 0; i < n; i++){
-        // this inner loop will print the spaces
+  int n;
+  cout << "Type the value of n : ";
+  cin >> n; 
+
+  for(int i = 0; i < n; i++){
         for(int j = 0; j < i; j++){
-            cout << " ";    
+        cout << " "; // This inner loop will print the empty spaces.
         }
-        //this innner lopp will print the num
-        for (int j = 0; j < n-i; j++){
-            cout << (i + 1);
+        for(int j = 0; j < n - i; j++)   {
+        cout << i + 1 << " "; // this second inner loop will print the num;
         }
-        cout << endl;
-    }
-    return 0;
+    cout << endl;
+  }
+  return 0;
 }
---
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/Programming/patterns/inverted_triangle/pattern_2/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    char ch = 'A';
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < n - i; j++){
+            cout << ch;
+        }
+        ch++;
+        cout << endl;
+    }
+    return 0;
+}
