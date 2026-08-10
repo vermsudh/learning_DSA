e3825e277ca1c7e015e0c612630239464f6171d4
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-10 19:06:22 +0530

    Add initial implementation of array basics in C++ with example code and notes image

---
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays_basics/coding/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int A[5];
+    A[0] = 12;
+    A[1] = 23;
+    A[2] = 34;
+
+    cout << sizeof(A) << endl;
+    cout << A[1];
+    printf("%d\n", A[2]); // you can use C syntax in cpp.
+    
+    int B[6] = {3,4,5,6,4,3};
+
+    for(int i = 0; i <= 5; i++){
+        cout << B[i] << " ";
+    }
+
+    return 0; // Please make sure you type everything before this return statment. 
+                // If you type anything after return 0, the compliler will ignore it. 
+}
