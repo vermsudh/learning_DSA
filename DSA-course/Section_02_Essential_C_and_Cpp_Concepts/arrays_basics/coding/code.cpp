6d88593040356236a63170fb420a5b54f343d86e
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-11 22:12:56 +0530

    Add various C++ function examples and patterns
    - Introduced multiple C++ function examples demonstrating memory usage, pass by value, and basic syntax.
    - Added practice questions for calculating the sum of digits and combinations (nCr).
    - Implemented various patterns including butterfly, Floyd's triangle, inverted triangle, square, and pyramid patterns.
    - Included images and explanations for better understanding of patterns.
    - Enhanced code structure and comments for clarity and educational purposes.

--- DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays_basics/coding/code.cpp
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays_basics/coding/code.cpp
@@ -14,16 +14,38 @@ int main(){
     
     int B[6] = {3,4,5,6,4,3};
 
-    for(int i = 0; i <= 5; i++){
-        cout << B[i] << " ";
-    }
+    // for(int i = 0; i <= 5; i++){
+    //     cout << B[i] << " ";
+    // }
     //How to write for each loop? 
 
+    for(int x : B){
+        cout << x << " ";
+    }
 
 
+    // Can we define the size of the array by taking an input from the user? 
+    // Yes
 
+    // Qyestion : WAP to take a input from the user to define the size of the array. 
+    int n;
+    cout << "Enter the value of n ? ";
+    cin >> n;
 
-    
+    int C[n]; // You can take the input from the user for defining the size of the array but you wont be able to 
+        // initlize the the values of the array. 
+
+    // You would have print mention the the value either one by one or run a loop to take the valye of the user.
+    int input;
+    for(int i = 0; i <= n; i++){
+        cout << "Enter the value of this inidce : " << endl;
+        cin >> input;
+        cout << "The value of " << i << " is : " << input;
+    }
+    for (int element : C) {
+        std::cout << element << " ";
+    }
+
     return 0; // Please make sure you type everything before this return statment. 
                 // If you type anything after return 0, the compliler will ignore it. 
 }
