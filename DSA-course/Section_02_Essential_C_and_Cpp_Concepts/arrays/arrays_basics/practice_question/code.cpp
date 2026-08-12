39456184d2b47180c657275f243e3e0a43f8d2b1
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-12 22:08:44 +0530

    arrya basics  covered

--- DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays/arrays_basics/practice_question/code.cpp
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays/arrays_basics/practice_question/code.cpp
@@ -17,13 +17,31 @@ int main(){
         arr[i] = {value};
     }
     for(int val : arr){
-        cout << val << " ";
+        cout << val << " " << endl;
     }
 
     // Part 2 would be to find out the smallest and the largest numbers in the array. 
     //Lets divide the problem.
 
-    
+    int max = arr[0];
+    int min = arr[0];
+    // We can also do one more thing
+    // we can initilize a variable called int smallest = INT_MAX
 
+    int smallest = INT_MAX;
+    for(int i = 0; i <= n - 1; i++){
+        if(arr[i] < smallest){
+            smallest = arr[i];
+        }
+    }
+    cout << "THe smallest number in this array is : " << smallest;
+
+    for(int i = 0; i <= n-1; i++){
+       if(max < arr[i]){
+            max = arr[i];
+       }
+    }
+    cout << "The maximum element in this array is : " << max;
+    
     return 0;
 }   
