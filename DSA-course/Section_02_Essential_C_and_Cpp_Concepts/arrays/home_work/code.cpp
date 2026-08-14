281a18f7ad5338a4e7cc0322f86151c757f79d15
vermsudh <vermsudh@gmail.com>
2026-08-14 14:43:23 +0530

    Add implementation for swapping max and min elements, and finding unique values in an array

--- DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays/home_work/code.cpp
+++ DSA-course/Section_02_Essential_C_and_Cpp_Concepts/arrays/home_work/code.cpp
@@ -14,8 +14,65 @@ void sumproductArray(int arr[], int size, int &sum,int &product){
 
 //WAF to swap the max and min number of the array
 void swapMaxMin(){
-    
+    int arr[5] = {5, 2, 9, 1, 7};
+
+    cout << "The original array is : ";
+
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+    int max = INT_MIN;
+    int min = INT_MAX;
+
+    int max_index, min_index;
+
+    for(int i = 0; i < 5; i++){
+        if(max < arr[i]){
+            max = arr[i];
+            max_index = i;
+        }
+        if(min > arr[i]){
+            min = arr[i];
+            min_index = i;
+        }
+    }
+
+    swap(arr[max_index], arr[min_index]);
+    cout << "The swapped arr of max and min is : ";
+
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
 }
+
+//WAF to print the unique values in the array.
+int findUniqueValue(){
+    int arr[] = {2, 4, 6, 6, 4 , 2, 9};
+
+    for(int i = 0; i < 7; i++){
+        cout << arr[i] <<" ";
+    }
+
+    cout << endl;
+    //using brute force
+    for(int i = 0; i < 7; i++ ){
+        int count = 0;
+        for(int j = 0; j < 7; j++){
+            if(arr[i] == arr[j] ){
+            count += 1;
+            }
+        }
+        if(count == 1){
+            cout << "The unique value in this array is : " << arr[i];
+        }
+    }
+    return -1;
+}
+
+//WAF to print the intersection of a given array.
 
 int main(){
     cout << "//WAP to calculate and product of all numbers in an array." << endl;
@@ -31,6 +88,21 @@ int main(){
 
     cout << "Sum: " << sum << endl;
     cout << "Product: " << product << endl;
+
+    cout << "----------------------------";
+    cout << endl;
+
+    swapMaxMin();
+
+    cout << endl;
+
+    cout << "----------------------------";
+
+    cout << endl;
+
+    findUniqueValue();
+
+    cout << endl;
 
     cout << "----------------------------";
 
