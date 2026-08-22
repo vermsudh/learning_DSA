b5298a010a39266e95cf6046952db3db7d26bbd2
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-08-22 19:43:09 +0530

    Add initial implementation for prime number analysis and majority element algorithms

--- Leetcodes_problems/question_169(Majority_Element)/bute_force/code.cpp
+++ Leetcodes_problems/question_169(Majority_Element)/bute_force/code.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
+    vector <int> nums = {2, 2, 2, 2, 1, 1, 1, 2, 2, 2};
+    int n = nums.size();
 
+    for(int i : nums){
+        int count = 1;
+        for(int j : nums){
+            if(i == j){
+                count += 1;
+            }
+        }
+        if(count > n/2){
+            cout << "Majority Element is : " << i << endl;
+            return 0;
+        }
+    }
+
+    cout << "No Majority Element was found";
+    return 0;
 }
 
--- Leetcodes_problems/question_169(Majority_Element)/sorted_array/code.cpp
+++ Leetcodes_problems/question_169(Majority_Element)/sorted_array/code.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
-
+#include <vector>
 
 
 int main()
 {
+    vector <int> nums = {2, 2, 2, 2, 1, 1, 1, 2, 2, 2};
+
+
+    
     return 0;
 }
