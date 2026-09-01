fdc477d32aeb5ec9660590536b9a026085b4a19b
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-09-01 23:43:24 +0530

    Add brute force and Kadane's algorithm implementations for maximum subarray problem

--- DSA-course/Section_04_Time_Space_Complexity/time_complexity/prime_numbers/code.cpp
+++ DSA-course/Section_04_Time_Space_Complexity/time_complexity/prime_numbers/code.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
+    int n;
+    for(int i = 0; i < n; i++){
+        if(n % i == 0){
+            cout << "Non Prime numbers";
+            break;
+        }
+    }
+
     return 0;
 }
