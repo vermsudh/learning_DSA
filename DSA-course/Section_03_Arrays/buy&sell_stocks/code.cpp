58a349b17404fa73bc643e6f24de6d71dad01b81
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-09-02 21:51:00 +0530

    Implement buyStock function and initialize prices in buy&sell_stocks code

--- DSA-course/Section_03_Arrays/buy&sell_stocks/code.cpp
+++ DSA-course/Section_03_Arrays/buy&sell_stocks/code.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
+#include <vector>
 
+vector<int> buyStock(vector<int> prices){
+    prices = {7, 1, 5, 3, 6, 4};
+
+    int buyStock = prices[0];
+    int maxProfit = 0;
+
+    for(int i = 0; i < prices.size(); i++){
+        
+    }
+
+}
 
 int main()
 {
