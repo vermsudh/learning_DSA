49d16ebaa14160388bfab30bb4b85b128070d6bc
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-09-07 13:19:18 +0530

    Add optimized approach for Move Zeros problem

---
+++ Leetcodes_problems/question_283-MoveZeros/optamized_approach/code.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+int main()
+{
+    vector<int> nums = {0, 1, 0, 3, 12};
+    int n = nums.size();
+
+    int left = 0;
+    int right = 0;
+
+    while (right < n) {
+
+        if (nums[right] != 0) {
+            swap(nums[left], nums[right]);
+            left++;
+        }
+
+        right++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
