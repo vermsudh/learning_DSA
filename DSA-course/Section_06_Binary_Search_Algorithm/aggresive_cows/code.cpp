436d8a5d8dcefd7d1984de75214be449dadff4ec
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-09-15 18:02:37 +0530

    Refactor checkMid function and update variable names for clarity in Aggressive Cows implementation

--- DSA-course/Section_06_Binary_Search_Algorithm/aggresive_cows/code.cpp
+++ DSA-course/Section_06_Binary_Search_Algorithm/aggresive_cows/code.cpp
@@ -2,23 +2,39 @@
 #include<vector>
 using namespace std;
 
-bool checkMid(vector<int> stalls, int c, int mid){
+bool checkMid(vector<int> stalls, int c, int minAllowedDistance){
 
-
+    int cowsCount = 1, lastStallPossible = 0;
+    
+    for(int i = 0; i < stalls.size() - 1; i++){
+        if(stalls[i] - lastStallPossible <= minAllowedDistance){
+            cowsCount ++;
+            lastStallPossible = stalls[i];
+        }
+        if(cowsCount == c){
+            return true;
+        }
+    }
+    return false;
 }
 
 int aggresiveCows(vector<int> stalls, int c){
-    int smallestValue = INT_MAX, largestValue = INT_MIN, ans = -1;
 
-    for(int i = 0; i < stalls.size(); i++){
-        smallestValue = min(smallestValue, stalls[i]);
-        largestValue = max(largestValue, stalls[i]);
-    }
-    int start = 1, end = largestValue - smallestValue;
+    int smallestValue = INT_MAX, largestValue = INT_MIN, ans = -1;
+    sort(stalls.begin(), stalls.end());
+    int start = 1, end = (stalls.size() - 1) - stalls[0];
 
     while(start <= end){
         int mid = start + (end - start)/2;
+
+        if(checkMid(stalls, c, mid)){
+            ans = mid;
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
     }
+    return ans;
 }
 
 int main()
