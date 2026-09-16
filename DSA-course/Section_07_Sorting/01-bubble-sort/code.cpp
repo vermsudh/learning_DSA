77d69a023ea1136c8f3029d6c25a9e0df8d77d24
sudhanshu verma <65793304+bunny-shinchan@users.noreply.github.com>
2026-09-16 12:23:33 +0530

    Add bubble sort implementation with optimized version

---
+++ DSA-course/Section_07_Sorting/01-bubble-sort/code.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//brute force which will have time complexity as O(n^2)
+void bubbleSort(int arr[], int n){ 
+    for(int i = 0; i < n -1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+//more optmazied apprach in orde to stop the sorting once the sorting is completed.
+void bubbleSortOptamized(int arr[], int n){ 
+    for(int i = 0; i < n -1; i++){
+        //optimization to make the loop stop when the array has been sorted. 
+        bool isSwap = false;
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+        if(!isSwap){
+            return;
+        }
+    }
+}
+
+
+
+int main()
+{
+    int n = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+
+    cout << "Before sort : ";
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr,n);
+
+    cout << " After sort : " ;
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
